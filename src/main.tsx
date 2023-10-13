ea7cec1cffaa64fc730bc2168f131906e092c6d8
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-10-13 15:46:16 -0600

    prettier all

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/css/index.css'
-import './styles/scss/index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/css/index.css';
+import './styles/scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    Hola Momento Oportuno
-  </React.StrictMode>,
-)
+  <React.StrictMode>Hola Momento Oportuno</React.StrictMode>
+);
