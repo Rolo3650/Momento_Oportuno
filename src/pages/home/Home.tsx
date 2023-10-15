92f547dc3977939446089514cfc847a015732453
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-10-15 08:56:25 -0600

    feat: :sparkles: add useDebounce

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Counter: {state?.counter}</p>
+      <p>Counter: {state.counter}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={reset}>Reset</button>
     </div>
