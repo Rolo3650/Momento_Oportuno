bffedd341bdd65c404761cb296d13c1ba4fb3876
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-10-19 09:01:09 -0600

    wellcome

---
+++ src/containers/modules/wellcome/WellcomeOne.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+const WellcomeOne: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="wellcome wellcome-one">
+      <div className="d-flex justify-content-between wellcome-body mx-auto">{children}</div>
+    </div>
+  );
+};
+
+export { WellcomeOne };
