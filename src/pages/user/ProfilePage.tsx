5a29132bdb6deb5dff9acffc91db26ddaa2ebff6
AlanJGG <alanguzmangutierrez2005@gmail.com>
2023-10-24 17:06:46 -0600

    profilepage

--- src/pages/user/ProfilePage.tsx
+++ src/pages/user/ProfilePage.tsx
@@ -5,11 +5,21 @@ const ProfilePage = () => {
     <LayoutOne>
       <div className="profile-toppart">
         <div className="profile-background">
-          <div className="profile-picture"></div>
+          <div className="profile-picture">
+            <img className="profile-userimg" src="/svg/icons/usr_frm.svg" />
+          </div>
         </div>
-
-        <div className="profile-username"></div>
-        <div className="profile-time"></div>
+        <div className="profile-info">
+          <div className="profile-usernamediv">
+            <h3 className="profile-username">user</h3>
+          </div>
+          <div className="profile-time">
+            <div className="profile-time_user-img">
+              <img className="profile-time_user-imguser" src="/svg/icons/usr_frm.svg" />
+            </div>
+            Miembro desde hace 3 semanas
+          </div>
+        </div>
       </div>
     </LayoutOne>
   );
