a318e48b1e1c980b870de124ed4fe8d6cc2c21e1
Arthur IDS <intdexsys@gmail.com>
2023-10-25 02:14:42 -0600

    Login register with console values

--- src/components/module/cards/CardLogReg.tsx
+++ src/components/module/cards/CardLogReg.tsx
@@ -1,6 +1,8 @@
 import React, { useState, ChangeEvent } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { TextFieldOne } from '../../inputs/text/TextFieldOne';
+import { PswFieldOne } from "../../inputs/password/PswFieldOne";
+import { Button, CircularProgress, FormControl } from '@mui/material';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
@@ -41,42 +43,81 @@ function a11yProps(index: number) {
 
 const CardLogRes = () => {
     const theme = useTheme();
-    const [lookingFor, setLookingFor] = useState('');
-    const onChangeLookingFor = (e: ChangeEvent<HTMLInputElement>) => {
-        setLookingFor(e.target.value);
+    const [ISusr, setISusr] = useState('');
+    const [ISpsw, setISpsw] = useState('');
+    const [loadingis, setLoadingIS] = useState<boolean>(false);
+    const [loadingrg, setLoadingRG] = useState<boolean>(false);
+    const onChangeISusr = (e: ChangeEvent<HTMLInputElement>) => {
+        setISusr(e.target.value);
     };
+    const onChangeISpsw = (e: ChangeEvent<HTMLInputElement>) => {
+        setISpsw(e.target.value);
+    };
+    const [RGusr, setRGusr] = useState('');
+    const [RGeml, setRGeml] = useState('');
+    const [RGtel, setRGtel] = useState('');
+    const [RGpsw, setRGpsw] = useState('');
+    const [RGnmc, setRGnmc] = useState('');
+    const [RGape, setRGape] = useState('');
+    const onChangeInput = (input:string, value:string) => {
+        switch (input) {
+            case "rgusername":
+                setRGusr(value);
+                break;
+            case "rgemail":
+                setRGeml(value);
+                break;
+            case "rgphone":
+                setRGtel(value);
+                break;
+            case "rgpassword":
+                setRGpsw(value);
+                break;
+            case "rgnamecomp":
+                setRGnmc(value);
+                break;
+            case "rgapellido":
+                setRGape(value);
+                break;
+            default:
+                break;
+        }
+    }
+
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
+
+    const onClickShowIS = () => {
+        setLoadingIS(true);
+        console.log(ISusr + "-" + ISpsw);
+        setTimeout(() => {
+            setLoadingIS(false);
+        },1000);
+    }
+    const onClickShowRG = () => {
+        setLoadingRG(true);
+        console.log(RGusr + "-" + RGeml + "-" + RGtel + "-" + RGpsw + "-" + RGnmc + "-" + RGape);
+        setTimeout(() => {
+            setLoadingRG(false);
+        },1000);
+    }
     return (
         <><div className='card-login card-login-logres'>
-            <Box sx={{ width: '100%'}}>
+            <Box sx={{ width: '100%' }}>
                 <Box>
                     <div className='card-login card-login-tabs'>
-                    <Tabs value={value} onChange={handleChange} variant="fullWidth" aria-label="basic tabs example">
-                        <Tab sx={{minWidth: '250px'}} className='card-login card-login-btns' label="Iniciar Sesión" {...a11yProps(0)} />
-                        <Tab sx={{minWidth: '250px'}} className='card-login card-login-btns' label="Regístrate" {...a11yProps(1)} />
-                    </Tabs>
+                        <Tabs value={value} onChange={handleChange} variant="fullWidth" aria-label="basic tabs example">
+                            <Tab sx={{ minWidth: '250px', textTransform: 'capitalize' }} className='card-login card-login-btns' label="Iniciar Sesión" {...a11yProps(0)} />
+                            <Tab sx={{ minWidth: '250px', textTransform: 'capitalize' }} className='card-login card-login-btns' label="Regístrate" {...a11yProps(1)} />
+                        </Tabs>
                     </div>
                 </Box>
                 <CustomTabPanel value={value} index={0}>
-                <div className='card-login card-login-cont'>
-                    <div className='card-login card-login-lrcont'>
-                        <TextFieldOne
-                            color={{
-                                variant: 'secondary',
-                                text: '#464748',
-                                field: theme.palette.secondary.main,
-                                backgroundColor: '#fff',
-                            }}
-                            text="Email o Nombre de usuario"
-                            icon={{ url: '/svg/icons/arr_frm.svg' }}
-                            value={lookingFor}
-                            onChange={onChangeLookingFor}
-                        ></TextFieldOne>
-                        <div className="mt-3">
+                    <div className='card-login card-login-cont'>
+                        <div className='card-login card-login-lrcont'>
                             <TextFieldOne
                                 color={{
                                     variant: 'secondary',
@@ -84,31 +125,49 @@ const CardLogRes = () => {
                                     field: theme.palette.secondary.main,
                                     backgroundColor: '#fff',
                                 }}
-                                text="Contraseña"
-                                icon={{ url: '/svg/icons/psw_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
+                                text="Email o Nombre de usuario"
+                                icon={{ url: '/svg/icons/arr_frm.svg' }}
+                                value={ISusr}
+                                onChange={onChangeISusr}
                             ></TextFieldOne>
+                            <div className="mt-3">
+                                <PswFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Contraseña"
+                                    icon={{ url: '/svg/icons/psw_frm.svg' }}
+                                    value={ISpsw}
+                                    onChange={onChangeISpsw}
+                                ></PswFieldOne>
+                            </div>
+                            <div className="mt-5">
+                                <FormControl fullWidth>
+                                    <Button
+                                        className="p-2"
+                                        variant="contained"
+                                        onClick={onClickShowIS}
+                                        sx={{
+                                            '& .text': {
+                                                paddingTop: '8px',
+                                                paddingBottom: '7px',
+                                            },
+                                        }}
+                                    >
+                                        {loadingis && <CircularProgress color="secondary" />}
+                                        {!loadingis && <span className="text">Iniciar Sesión</span>}
+                                    </Button>
+                                </FormControl>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={1}>
-                <div className='card-login card-login-cont'>
-                    <div className='card-login card-login-lrcont'>
-                        <TextFieldOne
-                            color={{
-                                variant: 'secondary',
-                                text: '#464748',
-                                field: theme.palette.secondary.main,
-                                backgroundColor: '#fff',
-                            }}
-                            text="Nombre de usuario*"
-                            icon={{ url: '/svg/icons/usr_frm.svg' }}
-                            value={lookingFor}
-                            onChange={onChangeLookingFor}
-                        ></TextFieldOne>
-                        <div className="mt-3">
+                    <div className='card-login card-login-cont'>
+                        <div className='card-login card-login-lrcont'>
                             <TextFieldOne
                                 color={{
                                     variant: 'secondary',
@@ -116,70 +175,101 @@ const CardLogRes = () => {
                                     field: theme.palette.secondary.main,
                                     backgroundColor: '#fff',
                                 }}
-                                text="Email*"
-                                icon={{ url: '/svg/icons/arr_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
-                            ></TextFieldOne>
-                        </div>
-                        <div className="mt-3">
-                            <TextFieldOne
-                                color={{
-                                    variant: 'secondary',
-                                    text: '#464748',
-                                    field: theme.palette.secondary.main,
-                                    backgroundColor: '#fff',
-                                }}
-                                text="Teléfono"
-                                icon={{ url: '/svg/icons/phone_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
-                            ></TextFieldOne>
-                        </div>
-                        <div className="mt-3">
-                            <TextFieldOne
-                                color={{
-                                    variant: 'secondary',
-                                    text: '#464748',
-                                    field: theme.palette.secondary.main,
-                                    backgroundColor: '#fff',
-                                }}
-                                text="Contraseña*"
-                                icon={{ url: '/svg/icons/psw_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
-                            ></TextFieldOne>
-                        </div>
-                        <div className="mt-3">
-                            <TextFieldOne
-                                color={{
-                                    variant: 'secondary',
-                                    text: '#464748',
-                                    field: theme.palette.secondary.main,
-                                    backgroundColor: '#fff',
-                                }}
-                                text="Nombre Completo*"
+                                text="Nombre de usuario*"
                                 icon={{ url: '/svg/icons/usr_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
+                                value={RGusr}
+                                onChange={e => onChangeInput("rgusername", e.target.value)}
                             ></TextFieldOne>
-                        </div>
-                        <div className="mt-3">
-                            <TextFieldOne
-                                color={{
-                                    variant: 'secondary',
-                                    text: '#464748',
-                                    field: theme.palette.secondary.main,
-                                    backgroundColor: '#fff',
-                                }}
-                                text="Apellido*"
-                                icon={{ url: '/svg/icons/usr_frm.svg' }}
-                                value={lookingFor}
-                                onChange={onChangeLookingFor}
-                            ></TextFieldOne>
+                            <div className="mt-3">
+                                <TextFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Email*"
+                                    icon={{ url: '/svg/icons/arr_frm.svg' }}
+                                    value={RGeml}
+                                    onChange={e => onChangeInput("rgemail", e.target.value)}
+                                ></TextFieldOne>
+                            </div>
+                            <div className="mt-3">
+                                <TextFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Teléfono"
+                                    icon={{ url: '/svg/icons/phone_frm.svg' }}
+                                    value={RGtel}
+                                    onChange={e => onChangeInput("rgphone", e.target.value)}
+                                ></TextFieldOne>
+                            </div>
+                            <div className="mt-3">
+                                <PswFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Contraseña*"
+                                    icon={{ url: '/svg/icons/psw_frm.svg' }}
+                                    value={RGpsw}
+                                    onChange={e => onChangeInput("rgpassword", e.target.value)}
+                                ></PswFieldOne>
+                            </div>
+                            <div className="mt-3">
+                                <TextFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Nombre Completo*"
+                                    icon={{ url: '/svg/icons/usr_frm.svg' }}
+                                    value={RGnmc}
+                                    onChange={e => onChangeInput("rgnamecomp", e.target.value)}
+                                ></TextFieldOne>
+                            </div>
+                            <div className="mt-3">
+                                <TextFieldOne
+                                    color={{
+                                        variant: 'secondary',
+                                        text: '#464748',
+                                        field: theme.palette.secondary.main,
+                                        backgroundColor: '#fff',
+                                    }}
+                                    text="Apellido*"
+                                    icon={{ url: '/svg/icons/usr_frm.svg' }}
+                                    value={RGape}
+                                    onChange={e => onChangeInput("rgapellido", e.target.value)}
+                                ></TextFieldOne>
+                            </div>
+                            <div className="mt-5">
+                                <FormControl fullWidth>
+                                    <Button
+                                        className="p-2"
+                                        variant="contained"
+                                        onClick={onClickShowRG}
+                                        sx={{
+                                            '& .text': {
+                                                paddingTop: '8px',
+                                                paddingBottom: '7px',
+                                            },
+                                        }}
+                                    >
+                                        {loadingrg && <CircularProgress color="secondary" />}
+                                        {!loadingrg && <span className="text">Regístrate</span>}
+                                    </Button>
+                                </FormControl>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </CustomTabPanel>
             </Box>
         </div></>
