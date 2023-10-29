e935124175b531518a445a5d2689f92a841a62a5
AlanJGG <alanguzmangutierrez2005@gmail.com>
2023-10-29 09:41:58 -0600

    favoritesPage

--- src/pages/user/Favorites.tsx
+++ src/pages/user/Favorites.tsx
@@ -1,31 +1,56 @@
 import React from 'react';
 import { LayoutThree } from '../../containers/layout/LayoutThree';
 import { useEffect } from 'react';
-import { useTheme } from '@mui/material';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppContext } from '../../context';
+import { useMyFavorites } from '../../hooks';
+import { ProductTwo } from '../../components/product/ProductTwo';
+import { Ad } from '../../api';
 
-interface Props {}
+interface Props { }
 
 const Favorites: React.FC<Props> = () => {
 
-  const { pathname } = useLocation();
-  const navigateTo = useNavigate();
-  const theme = useTheme();
+  const { data: favorites, isLoading: loadFav } = useMyFavorites();
+
+
   const { state } = useAppContext();
 
   useEffect(() => {
+    console.log("favorites", favorites);
+    console.log("favoriteslenght", favorites?.data.length)
     if (!state?.userState?.token) {
       window.location.assign('/');
     }
   }, [state.userState]);
-  
+
+  const FavoritesListEmpty = () => {
+    return (
+      <div className="favorites-empty">
+        <div className="favorites-empty-title">No has agregado publicaciones a favoritos.</div>
+        <div className="favorites-empty-img"><img src="/public/svg/icons/empty_box.svg" /></div>
+      </div>
+    );
+  };
+
+  const FavoritesList = () => {
+    return (
+      <div>
+        <h1 className="title text text-font-georgia fw-bold fs-2 text-color-5">
+          Favoritos
+        </h1>
+        <div>
+          {loadFav && <p>Loading...</p>}
+          {favorites && favorites.data.map((fav) => (<div><ProductTwo product={fav}/></div>))}
+        </div>
+      </div>
+
+    );
+  };
+
   return (
     <LayoutThree>
-      <h1 className="title text text-font-georgia fw-bold fs-2 text-color-5">
-        Favoritos
-      </h1>
-      <div>
+      <div className="favorites-container">
+        {favorites && favorites.data.length > 0 ? <FavoritesList /> : <FavoritesListEmpty />}
       </div>
     </LayoutThree>
   );
