523b1d14d938a413e4b337cb71ca87d335669d16
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-10-30 19:02:00 -0600

    fix

--- src/pages/user/AddNewAd.tsx
+++ src/pages/user/AddNewAd.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { LayoutThree } from '../../containers/layout/LayoutThree';
+import { FormOne } from '../../components/form/FormOne';
 
 interface Props {}
 
 const AddNewAd: React.FC<Props> = () => {
   return (
     <LayoutThree>
-      <h1 className="title text text-font-georgia fw-bold fs-2 text-color-5">Publicar Anuncio</h1>
+      <h1 className="title text text-font-georgia fw-bold fs-2 text-color-5">
+        Publicar Anuncio
+      </h1>
+      <div className='mt-4'>
+        <FormOne />
+      </div>
     </LayoutThree>
   );
 };
