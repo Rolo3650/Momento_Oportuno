ebeca3f74d3d003ff0a4c5f91ce2724d006f33f0
AlanJGG <alanguzmangutierrez2005@gmail.com>
2023-11-07 23:26:10 -0600

    directories connect

--- src/components/formDirectory/FormDirectoryOne.tsx
+++ src/components/formDirectory/FormDirectoryOne.tsx
@@ -1,37 +1,39 @@
 import { TextFieldOne } from '../inputs/text/TextFieldOne';
 import { DropdownOne } from '../inputs/dropdown/DropdownOne';
-import { useTheme } from '@mui/material';
+import { IconButton, useTheme } from '@mui/material';
 import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 import WebIcon from '@mui/icons-material/Web';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import WorkIcon from '@mui/icons-material/Work';
 import BackupIcon from '@mui/icons-material/Backup';
 import PlaceIcon from '@mui/icons-material/Place';
+import ClearIcon from '@mui/icons-material/Clear';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import { useGetStates } from '../../hooks/querys/useStates';
+// import { useCreateDirectorio } from '../../hooks/directorios';
 import { GeneralButton } from '../inputs/buttons/GeneralButton';
-import { useState, useEffect } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { DirectoriesServices } from '../../api';
 import { useAppContext } from '../../context';
-import { State } from '../../api';
+// import { State } from '../../api';
 import { ThemeTwo } from '../../themes/ThemeTwo';
 import { Button } from '@mui/joy';
-// interface Option {
-//   label: string;
-//   value: number | string;
-//   quantity?: number;
-// }
+import Swal from 'sweetalert2';
+import { CreateDirectorio } from '../../api';
 
-interface DirectoryAd {
+interface DirectoryParams {
   title: string;
-  businessType: string;
+  type: string;
   address: string;
-//   img: string | null;
-  schedule: string;
+  hours: string;
   email: string;
   phone: string;
-  website: string;
-  state?: State;
+  thumbnail: string;
+  state_id: number;
+  user_id?: number;
 }
+
+type Imgs = string | ArrayBuffer | null;
 
 const FormDirectoryOne = () => {
   const { state } = useAppContext();
@@ -39,18 +41,22 @@ const FormDirectoryOne = () => {
 
   const { data } = useGetStates();
 
-  const initialDirectoryAd: DirectoryAd = {
+  const initialDirectoryAd: DirectoryParams = {
     title: '',
-    businessType: '',
+    type: '',
     address: '',
-    schedule: '',
+    // img: null,
+    hours: '',
     email: '',
     phone: '',
-    website: '',
-    state: data?.data[0],
+    thumbnail: '',
+    state_id: 0,
+    user_id: state.userState?.user.id,
   };
 
-  const [formData, setFormData] = useState<DirectoryAd>(initialDirectoryAd);
+  const [formData, setFormData] = useState<DirectoryParams>(initialDirectoryAd);
+  const [imgFile, setImgFile] = useState<File | null>(null);
+  const [selectedImage, setSelectedImage] = useState<Imgs>(null);
 
   useEffect(() => {
     if (!state?.userState?.token) {
@@ -58,38 +64,148 @@ const FormDirectoryOne = () => {
     }
   }, [state.userState]);
 
+  useEffect(() => {
+    redFiles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [imgFile]);
+
   const handleInputChange = (name: string, value: string | number) => {
-    if (name === 'state') {
+    if (name === 'state_id') {
       const selectedState = data?.data?.find((city) => city.id === value);
-      setFormData({ ...formData, state: selectedState });
+      setFormData({ ...formData, state_id: selectedState?.id });
     } else {
       setFormData({ ...formData, [name]: value });
     }
   };
 
-//   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-//     if (e.target.files?.length) {
-//       let files: File[] = [];
-//       if (newAdForm?.imgs?.length) {
-//         files = [...newAdForm.imgs];
-//       }
-//       for (let i = 0; i < e.target.files.length; i++) {
-//         if (files?.length < newAdForm.extraImgs.quantity) {
-//           files.push(e.target.files[i]);
-//         } else {
-//           Swal.fire(
-//             '¿Necesitas más imágenes?',
-//             'Haz alcanzado el límite de imagens, para subir más, selecciona la cantidad de imágenes que necesitas en el apartado de arriba.',
-//             'warning'
-//           );
-//         }
-//       }
-//       setFormData({ imgs: [...files] });
-//     }
-//   };
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files?.length === 1) {
+      setImgFile(e.target.files[0]);
+    } else {
+      Swal.fire(
+        'Sólo puedes subir una imagen',
+        'Sólo puedes subir una imagen para el directorio',
+        'warning'
+      );
+    }
+  };
 
-  const onCreateMicrositio = () => {
+  const promise = (img: File) => {
+    return new Promise<string | ArrayBuffer | null>((resolve) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target) {
+          resolve(e.target.result);
+        } // Asignamos la URL de la imagen al estado
+      };
+
+      reader.readAsDataURL(img);
+    });
+  };
+
+  const redFiles = async () => {
+    if (imgFile) {
+      const url = await promise(imgFile);
+      setSelectedImage(url);
+    } else {
+      setSelectedImage(null);
+    }
+  };
+
+  const onDelete = () => {
+    setFormData((prevData) => ({
+      ...prevData,
+      img: null,
+    }));
+  };
+
+  const onCreateMicrositio = async () => {
     console.log(formData);
+    let error: boolean = false;
+    let message: string = '';
+
+    if (!formData.title || formData.title == '') {
+      message = 'Ingresa un titulo a la';
+      error = true;
+    }
+    if (!formData.type || formData.type == '') {
+      message = 'Ingresa un tipo de negocio';
+      error = true;
+    }
+    if (!formData.address || formData.address == '') {
+      message = 'Ingresa una dirección';
+      error = true;
+    }
+    if (!imgFile) {
+      message = 'Ingresa una imagen';
+      error = true;
+    }
+    if (!formData.hours || formData.hours == '') {
+      message = 'Ingresa un horario de funcionamiento';
+      error = true;
+    }
+    if (
+      !formData.phone ||
+      formData.phone == '' ||
+      formData.phone.length != 10
+    ) {
+      message = 'Ingresa un teléfono de 10 caractéres';
+      error = true;
+    }
+    if (!formData.thumbnail || formData.thumbnail == '') {
+      message = 'Ingresa una dirección de sitio web';
+      error = true;
+    }
+    if (!formData.state_id && formData.state_id == 0) {
+      message = 'Selecciona un estado';
+      error = true;
+    }
+
+    if (error) {
+      Swal.fire('Error', message, 'error');
+    } else {
+      const obj: CreateDirectorio = {
+        title: formData.title,
+        state_id: formData.state_id,
+        type: formData.type,
+        hours: formData.hours,
+        address: formData.address,
+        phone: formData.phone,
+        email: formData.email,
+        user_id: formData.user_id,
+        thumbnail: formData.thumbnail,
+      };
+      let error_2: boolean = false;
+      const res1 = await DirectoriesServices.createDirectorio(obj);
+      // console.log("RES1", res1);
+      if (imgFile && res1) {
+        const res2 = await DirectoriesServices.uploadImage({
+          file: imgFile,
+          id: res1.id,
+        });
+        if (!res2.data.id) {
+          error_2 = true;
+        }
+      } else {
+        error_2 = true;
+      }
+
+      if (error_2) {
+        Swal.fire(
+          'Error',
+          'Error al conectar con el servidor, por favor intentalo más tarde',
+          'error'
+        );
+      } else {
+        Swal.fire('¡Listo!', 'Anuncio creado correctamente', 'success').then(
+          () => {
+            // setNewAdForm({ responseForm: response_1 });
+            // navigateTo('/panel/listings/pago');
+          }
+        );
+      }
+
+    }
   };
 
   return (
@@ -119,8 +235,8 @@ const FormDirectoryOne = () => {
             }}
             text="Tipo de negocio: Ej. Ferretería"
             icon={{ mui: <WorkIcon color="secondary" /> }}
-            onChange={(e) => handleInputChange('businessType', e.target.value)}
-            value={formData.businessType}
+            onChange={(e) => handleInputChange('type', e.target.value)}
+            value={formData.type}
           />
         </div>
         <div className="directory-form-item">
@@ -151,35 +267,58 @@ const FormDirectoryOne = () => {
           />
         </div>
         <div className="directory-form-item">
-        <ThemeTwo>
-          <Button
-            sx={{
-              width: '100%',
-            }}
-            className="py-5 fs-3"
-            component="label"
-            role={undefined}
-            tabIndex={-1}
-            variant="outlined"
-            color="primary"
-            startDecorator={
-              <BackupIcon
-                sx={{
-                  fontSize: '40px',
-                }}
+          <div className="imgs">
+            {selectedImage ? (
+              <div className="img-container d-flex align-items-center justify-content-center position-relative">
+                <img src={selectedImage?.toString() ?? ''} alt="img" />
+                <IconButton
+                  sx={{
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    background: theme.palette.secondary.main,
+                    color: 'white',
+                    ':hover': {
+                      background: theme.palette.secondary.main,
+                      color: 'white',
+                    },
+                  }}
+                  onClick={() => onDelete()}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </div>
+            ) : null}
+          </div>
+          <ThemeTwo>
+            <Button
+              sx={{
+                width: '100%',
+              }}
+              className="py-5 fs-3"
+              component="label"
+              role={undefined}
+              tabIndex={-1}
+              variant="outlined"
+              color="primary"
+              startDecorator={
+                <BackupIcon
+                  sx={{
+                    fontSize: '40px',
+                  }}
+                />
+              }
+            >
+              Subir una Imagen
+              <input
+                type="file"
+                className="d-none"
+                multiple
+                accept="image/*"
+                onChange={onChange}
               />
-            }
-          >
-            Subir una Imagen
-            <input
-              type="file"
-              className="d-none"
-              multiple
-              accept="image/*"
-            //   onChange={onChange}
-            />
-          </Button>
-        </ThemeTwo>
+            </Button>
+          </ThemeTwo>
         </div>
         <div className="directory-form-item">
           <TextFieldOne
@@ -191,8 +330,8 @@ const FormDirectoryOne = () => {
             }}
             text="Horarios de funcionamiento: Ej. 8:00am - 5:00pm"
             icon={{ mui: <AccessTimeIcon color="secondary" /> }}
-            onChange={(e) => handleInputChange('schedule', e.target.value)}
-            value={formData.schedule}
+            onChange={(e) => handleInputChange('hours', e.target.value)}
+            value={formData.hours}
           />
         </div>
         <div className="directory-form-item">
@@ -233,8 +372,8 @@ const FormDirectoryOne = () => {
             }}
             text="Sitio Web"
             icon={{ mui: <WebIcon color="secondary" /> }}
-            onChange={(e) => handleInputChange('website', e.target.value)}
-            value={formData.website}
+            onChange={(e) => handleInputChange('thumbnail', e.target.value)}
+            value={formData.thumbnail}
           />
         </div>
         <div className="directory-form-item">
@@ -266,7 +405,7 @@ const FormDirectoryOne = () => {
               value: city?.id,
               quantity: 1,
             }))}
-            onChange={(option) => handleInputChange('state', option.value)}
+            onChange={(option) => handleInputChange('state_id', option.value)}
           />
         </div>
         <div className="directory-form-item">
