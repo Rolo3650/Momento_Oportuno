9aea9026ecee7e245510d0c7afad203b0d2bfaa8
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-11-09 01:45:50 -0600

    fix

--- src/components/order/OrderOne.tsx
+++ src/components/order/OrderOne.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router';
 
 interface Props {
@@ -10,54 +10,37 @@ interface Props {
 }
 
 const OrderOne: React.FC<Props> = ({ name, desc, total, id }) => {
-  useEffect(() => {
-    console.log({
-      name,
-      desc,
-      total,
-      id,
-    });
-    // console.log(typeof desc)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const navigateTo = useNavigate();
   return (
     <div className={`order order-one p-3`}>
       <div className="name text text-font-l-d fw-bold fs-5 pb-3 text-color-5">
-        {/* Orden {`${id}`}{' '}
+        Orden {id}{' '}
         {name
           ?.replace('listing', 'anuncio')
-          ?.replace('directory', 'directorio')
-          ?.replace('microsite', 'micrositio')} */}
+          .replace('directory', 'directorio')
+          .replace('microsite', 'micrositio')}
       </div>
-      {/* <div className="desc text text-rubik pb-2">
-        {`${desc
+      <div className="desc text text-rubik pb-2">
+        {desc
           ?.replace('Order for', 'Orden para')
-          ?.replace('listing', 'anuncio')
-          ?.replace('directory', 'directorio')
-          ?.replace('microsite', 'micrositio')}`}
-      </div> */}
+          .replace('listing', 'anuncio')
+          .replace('directory', 'directorio')
+          .replace('microsite', 'micrositio')}
+      </div>
 
       <div className="price text text-color-primary fw-bold fs-4">
-        {/* Total ${' '}
-        {`${total
-        // .toLocaleString('es-MX', {
-        //   minimumFractionDigits: 2,
-        //   maximumFractionDigits: 2,
-        // })
-        }`} */}
+        Total ${' '}
+        {total?.toLocaleString('es-MX', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
         <div className="w-100 mt-3 d-flex justify-content-center btn-container">
           <Button
             variant="contained"
             className="mx-auto"
             color="secondary"
             onClick={() => {
-              // // navigateTo('/comprobante/' + id);
-              // navigateTo('/comprobante/');
-              if (id) {
-                navigateTo(`/comprobante/${id ?? 0}`);
-              }
+              navigateTo('/comprobante/' + id);
             }}
           >
             Ver Detalle
