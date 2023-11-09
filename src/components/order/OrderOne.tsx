beac1ddf71bba167d4ccd517865d702f41c8b154
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-11-08 23:54:48 -0600

    build

--- src/components/order/OrderOne.tsx
+++ src/components/order/OrderOne.tsx
@@ -24,7 +24,7 @@ const OrderOne: React.FC<Props> = ({ name, desc, total, id }) => {
   return (
     <div className={`order order-one p-3`}>
       <div className="name text text-font-l-d fw-bold fs-5 pb-3 text-color-5">
-        Orden {id ? id?.toString() : ''}{' '}
+        Orden {`${id}`}{' '}
         {name
           ?.replace('listing', 'anuncio')
           ?.replace('directory', 'directorio')
