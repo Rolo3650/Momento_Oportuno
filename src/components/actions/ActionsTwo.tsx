4d26e4aae4763b20432f5d55ee9ca64a06d05e8f
AlanJGG <alanguzmangutierrez2005@gmail.com>
2024-01-18 17:37:21 -0600

    fix actions

--- src/components/actions/ActionsTwo.tsx
+++ src/components/actions/ActionsTwo.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { IconButton } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+// import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 // import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import { Ad } from '../../api';
@@ -9,6 +9,8 @@ import { LogResForm } from '../modals/LogResForm';
 import { useAddFavorite, useRemoveFavorite } from '../../hooks';
 import ShareIcon from '@mui/icons-material/Share';
 import { useAppContext } from '../../context';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 interface Props {
   product: Ad | null;
@@ -21,6 +23,7 @@ const ActionsTwo: React.FC<Props> = ({ product, fav }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [compare, _SetCompare] = useState<boolean>(false);
   const [isModalLogResOpen, setIsModalLogResOpen] = useState<boolean>(false);
+  const [openToast, setOpenToast] = useState<boolean>(false);
 
   const { mutate: add, isLoading: load1 } = useAddFavorite();
   const { mutate: remove, isLoading: load2 } = useRemoveFavorite();
@@ -34,11 +37,12 @@ const ActionsTwo: React.FC<Props> = ({ product, fav }) => {
       'https://momento-oportuno.vercel.app/ad/' +
         (state?.adSingleState?.ad?.id ?? 0)
     );
+    setOpenToast(true);
   };
 
-  const onClickView = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-  };
+  // const onClickView = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.stopPropagation();
+  // };
 
   const toggle = (id: number) => {
     if (fav) {
@@ -65,7 +69,7 @@ const ActionsTwo: React.FC<Props> = ({ product, fav }) => {
 
   return (
     <div>
-      <IconButton
+      {/* <IconButton
         sx={{
           border: `1px solid ${'#FD8A2A'}`,
           transition: '.3s ease-in-out',
@@ -81,7 +85,7 @@ const ActionsTwo: React.FC<Props> = ({ product, fav }) => {
             fontSize: '25px',
           }}
         />
-      </IconButton>
+      </IconButton> */}
       <IconButton
         className="ms-2"
         sx={{
@@ -130,6 +134,15 @@ const ActionsTwo: React.FC<Props> = ({ product, fav }) => {
         show={isModalLogResOpen}
         onHide={() => setIsModalLogResOpen(false)}
       />
+      <Snackbar
+        open={openToast}
+        autoHideDuration={3000}
+        onClose={() => setOpenToast(false)}
+      >
+        <Alert severity="success" variant="filled" sx={{ width: '100%' }}>
+          Link copiado en el portapapeles.
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
