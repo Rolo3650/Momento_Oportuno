1578c94c2a991a79e76a95bb73b5094b4446f545
Luis <luis.pina.juarez@gmail.com>
2024-02-13 13:56:33 -0600

    remove (.) & fix img

--- src/components/module/cards/CardOne.tsx
+++ src/components/module/cards/CardOne.tsx
@@ -6,7 +6,7 @@ const CardOne = () => {
   return (
     <div className="card-custom card-custom-one">
       <div className="card-custom-heading fw-bold text text-color-5 py-4">
-        Encuentra cualquier cosa <br /> a tu alrededor.
+        Encuentra cualquier cosa <br /> a tu alrededor
       </div>
       <div className="text text-font-helvetica text-description">
         Publicar un anuncio es fácil, ¡sólo toma unos sencillos pasos! Elige la
