7cb280cd2bd4f5843ae90d17cd5e388d6385ff20
Luis <luis.pina.juarez@gmail.com>
2024-02-19 15:03:54 -0600

    fiix br

--- src/components/module/cards/CardThree.tsx
+++ src/components/module/cards/CardThree.tsx
@@ -5,7 +5,7 @@ const CardThree = () => {
   return (
     <div className="card-custom card-custom-three ps-5">
       <div className="card-custom-heading fw-bold text text-color-5 py-4">
-        Encuentra cualquier cosa <br /> a tu alrededor
+        Encuentra cualquier cosa a tu alrededor
       </div>
       <div className="text text-font-georgia text-description">
         Publicar un anuncio es fácil, ¡sólo toma unos sencillos pasos! Elige la
